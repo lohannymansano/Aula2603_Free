20ffe638ec3ca816405ef10036621b97f2eae4c7
lohannymansano <mansanolohannybeatriz@gmail.com>
2025-03-26 12:56:17 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './components/style.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <h3>Lohanny Beatriz Mansano Santos</h3>
+
+    <img src='https://img.elo7.com.br/product/zoom/32AEC91/capa-para-painel-as-super-poderosas-docinho-lona-capa-para-painel.jpg'></img>
+    
+    <h4>Sobre:</h4>
+
+    <p>Lohanny é aluna do Senai Zerbini e da outra escola, quer cursar
+       veterinaria na puc ou biologia na unicamp.
+       Gosto de dormir e dos meus animais</p>
+     
+    </div>
+  );
+}
+export default App;
